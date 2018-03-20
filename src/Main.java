a896abf919642e19dab613fa5b3975c2bbdb817f
LDanchenko <2l-u-b-o-v@ukr.net>
2018-03-20 14:24:37 +0200

    тестить создание остальных таблиц

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.sql.*;
+import java.util.logging.*;
+
+
+public class Main {
+
+
+
+
+    public static void main(String[] args) throws SQLException {
+
+        Connect conn = new Connect();
+        conn.getConnect();
+
+    }
+}
