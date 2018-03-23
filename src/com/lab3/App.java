0204825f3ee84ecd949f9e99c1106febcfe78d42
LDanchenko <2l-u-b-o-v@ukr.net>
2018-03-23 11:21:36 +0200

    внешний вид

--- src/com/lab3/App.java
+++ src/com/lab3/App.java
@@ -8,7 +8,7 @@ import java.sql.*;
 
 public class App {
     private JTable table1;
-    private  JButton button1;
+    private JButton button1;
     private JPanel panelMain;
     private JTabbedPane tabbedPane1;
     private JTable table2;
@@ -16,6 +16,7 @@ public class App {
     private JTable table4;
     private JTable table5;
     private JTable table6;
+    private JButton button2;
     private JTable table7;
 
 
@@ -35,12 +36,14 @@ public class App {
         //выводим магазины
         table6.setModel(showData(statement, "SELECT * FROM stores"));
         //выводим колличество товаров в магазине
+
+
         table7.setModel(showData(statement, "SELECT c.id, c.count, s.name AS store, g.name AS goods FROM count c INNER JOIN stores s ON c.store = s.id INNER JOIN goods g ON c.goods = g.id "));
 //выводим заказы
         table3.setModel(showData(statement, "SELECT o.id, o.town,  o.representation, o.route,  a.name AS address, g.name AS goods, c.name AS client, t.name AS transport, st.name AS store, s.name AS status  " +
                 "FROM orders o INNER JOIN addresses a ON o.address = a.id INNER JOIN goods g ON o.goods = g.id INNER JOIN clients c ON o.client = c.id  INNER JOIN transport t ON o.transport = t.id  " +
                 " INNER JOIN stores st ON o.store = st.id INNER JOIN status s ON o.status = s.id"));
-       // table3.setModel(showData(statement, "SELECT a.name AS address FROM orders o INNER JOIN addresses a ON o.address = a.id"));
+        // table3.setModel(showData(statement, "SELECT a.name AS address FROM orders o INNER JOIN addresses a ON o.address = a.id"));
 
 //закрываем подключение
         con.close();
@@ -58,20 +61,20 @@ public class App {
     //тут выводим данные в таблицы
 
     public DefaultTableModel showData(Statement statement, String Query) throws SQLException {
-        DefaultTableModel tableModel = new DefaultTableModel(0,0);
+        DefaultTableModel tableModel = new DefaultTableModel(0, 0);
         ResultSet resultSet = statement.executeQuery(Query);
         // из метаданных узнаем колличество столбцов строк и названия столбцов
-        ResultSetMetaData metaData =  resultSet.getMetaData();
+        ResultSetMetaData metaData = resultSet.getMetaData();
         int columnCount = metaData.getColumnCount();
         //добавляем столбы в модель таблицы
-        for (int i = 1; i<=columnCount; i++){
+        for (int i = 1; i <= columnCount; i++) {
             tableModel.addColumn(metaData.getColumnName(i));
         }
         //добавляем строки в модель таблицы
         String[] row = new String[columnCount];
-        while (resultSet.next()){
-            for (int i = 1; i<=columnCount; i++){
-                row [i-1] = resultSet.getString(i);
+        while (resultSet.next()) {
+            for (int i = 1; i <= columnCount; i++) {
+                row[i - 1] = resultSet.getString(i);
             }
             tableModel.addRow(row);
         }
@@ -80,19 +83,14 @@ public class App {
     }
 
 
-
-
-
-
     public static void main(String[] args) throws SQLException {
         JFrame frame = new JFrame("App"); //работа с формой
         frame.setContentPane(new App().panelMain); // отобразить контент элемента панель
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-       App app = new App();
 
-
+        App app = new App();
 
 
     }
